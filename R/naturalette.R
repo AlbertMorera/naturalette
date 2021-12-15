a743014ff51cf91653d83588a642b08440a1399b
AlbertMorera <moreora.marra@gmail.com>
2021-12-15 16:07:32 +0100

    new description

---
+++ R/naturalette.R
@@ -0,0 +1,10 @@
+#' naturalette package
+#'
+#' Palettes inspired by National Parks
+#'
+#' @docType package
+#'
+#' @author Albert Morera \email{morera.marra@gmail.com}
+#'
+#' @name naturalette
+NULL
